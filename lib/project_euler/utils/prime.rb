16b3c2a0d25fdc7493c1d23404efd3431003ae80
Stephen Potenza <potenza@gmail.com>
2012-01-05 23:19:49 -0800

    comments

--- lib/project_euler/utils/prime.rb
+++ lib/project_euler/utils/prime.rb
@@ -2,6 +2,7 @@ module ProjectEuler
   module Utils
     class Prime
       def self.upto n
+        # based on sieve of eratosthenes
         primes = (2..n).to_a.unshift(nil, nil)
         (2..Math.sqrt(n)).each do |i|
           next if i > 2 && i % 2 == 0 # can safely skip factors of 2
